5cb4b2dbaa6af23190c73f77a318f72bbf2781e5
Michael Calcinai <michael@calcin.ai>
2016-09-12 15:37:50 +0200

    Added i2c stub

---
+++ src/PHPi/Peripheral/I2C.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package    calcinai/phpi
+ * @author     Michael Calcinai <michael@calcin.ai>
+ */
+
+namespace Calcinai\PHPi\Peripheral;
+
+use Calcinai\PHPi\Board;
+
+class I2C extends AbstractPeripheral {
+
+    public function __construct(Board $board) {
+        throw new \Exception('I2C not implemented');
+    }
+
+}
