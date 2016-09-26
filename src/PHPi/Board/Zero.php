86d1a9ee2df7b3af1548c29b6f0d9c7d5da6a30a
diego <diego@sirdiego.de>
2016-09-26 21:19:38 +0200

    Add J8 header and board name to Zero
    In addition to the mapping inside the factory we need some more
    changes to use the zero.

--- src/PHPi/Board/Zero.php
+++ src/PHPi/Board/Zero.php
@@ -13,5 +13,9 @@ use Calcinai\PHPi\Board\Feature;
 class Zero extends Board {
 
     use Feature\SoC\BCM2835;
+    use Feature\Header\J8;
 
+    public static function getBoardName() {
+        return 'Zero';
+    }
 }
