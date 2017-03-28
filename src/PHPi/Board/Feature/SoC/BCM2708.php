8398743e4be8a78bc6a533ad8d103194694d9884
Thomas Göttgens <tgoettgens@gmail.com>
2017-03-28 16:17:51 +0200

    Create BCM2708.php

---
+++ src/PHPi/Board/Feature/SoC/BCM2708.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package    calcinai/phpi
+ * @author     Michael Calcinai <michael@calcin.ai>
+ */
+namespace Calcinai\PHPi\Board\Feature\SoC;
+trait BCM2708
+{
+    //From what I understand there's not a lot of difference between the SoC except the CPU
+    use BCM2835;
+}
